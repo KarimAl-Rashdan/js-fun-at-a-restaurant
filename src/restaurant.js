32215d10ffcdc40d218b09e25ba7a6b05a6c60e9
karimy <karim.alrashdan14@gmail.com>
2022-10-13 17:13:26 -0600

    Complete createRestaurant function

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,7 +1,16 @@
-
+function createRestaurant(restaurantName) {
+  var restaurant = {}
+  restaurant.name = restaurantName
+  restaurant.menus = {
+    breakfast: [],
+    lunch: [],
+    dinner: [],
+  }
+  return restaurant
+}
 
 module.exports = {
-  // createRestaurant, 
+  createRestaurant, 
   // addMenuItem,
   // removeMenuItem
 }
