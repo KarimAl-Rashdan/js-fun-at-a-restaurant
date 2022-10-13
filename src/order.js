052ceb41e31f7f19452fa21cd3ba922556991c72
karimy <karim.alrashdan14@gmail.com>
2022-10-12 22:27:26 -0600

    Complete refundOrder function

--- src/order.js
+++ src/order.js
@@ -3,13 +3,31 @@ function takeOrder(order, orderList) {
   return orderList.push(order)
   }
 }
+// var uniqueNumber = orderList[i].orderNumber
+
+function refundOrder(orderNum, orderList) {
+  for (var i = 0; i < orderList.length; i++) {//i is the idex num of each element 0, 1, 2
+    // console.log("HERE", orderList[i])
+    var uniqueNumber = orderList[i].orderNumber
+    // console.log("orderNumber for each object", uniqueNumber)
+  
+  if(uniqueNumber === orderNum){
+    var newList = orderList.splice(i, 1)
+    return newList
+    
+
+  }
+  }
+    
+}
+
 
 
 
 
 module.exports = {
   takeOrder,
-  // refundOrder,
+  refundOrder,
   // listItems,
   // searchOrder
 }
