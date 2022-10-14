a258b68760002880ddcb865a48a5d9358dd5d122
karimy <karim.alrashdan14@gmail.com>
2022-10-14 15:56:32 -0600

    Complete addMenuItem function

--- src/restaurant.js
+++ src/restaurant.js
@@ -8,9 +8,20 @@ function createRestaurant(restaurantName) {
   }
   return restaurant
 }
-
+function addMenuItem(restaurant, itemDetails) {
+  if(itemDetails.type === "lunch" && !restaurant.menus.lunch.includes(itemDetails)){
+    restaurant.menus.lunch.push(itemDetails)
+  } else if (itemDetails.type === "breakfast" &&  !restaurant.menus.breakfast.includes(itemDetails)
+  ) {
+    restaurant.menus.breakfast.push(itemDetails)
+  } else if (itemDetails.type === "dinner" &&  !restaurant.menus.dinner.includes(itemDetails)
+  ) {
+    restaurant.menus.dinner.push(itemDetails)
+  } 
+  return restaurant.menus
+}
 module.exports = {
   createRestaurant, 
-  // addMenuItem,
+  addMenuItem,
   // removeMenuItem
 }
