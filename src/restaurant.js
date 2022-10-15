dbaec4795c9b98c35a35ad75d96e90e7e32eff37
karimy <karim.alrashdan14@gmail.com>
2022-10-14 23:56:49 -0600

    Refactor restaurant.js file

--- src/restaurant.js
+++ src/restaurant.js
@@ -1,38 +1,39 @@
 function createRestaurant(restaurantName) {
-  var restaurant = {}
-  restaurant.name = restaurantName
-  restaurant.menus = {
-    breakfast: [],
-    lunch: [],
-    dinner: [],
-  }
-  return restaurant
-}
+  var restaurant = {
+    name: restaurantName,
+    menus: {
+      breakfast: [],
+      lunch: [],
+      dinner: [],
+    },
+  };
+  return restaurant;
+};
+
 function addMenuItem(restaurant, itemDetails) {
-  if(itemDetails.type === "lunch" && !restaurant.menus.lunch.includes(itemDetails)){
-    restaurant.menus.lunch.push(itemDetails)
-  } else if (itemDetails.type === "breakfast" &&  !restaurant.menus.breakfast.includes(itemDetails)
-  ) {
-    restaurant.menus.breakfast.push(itemDetails)
-  } else if (itemDetails.type === "dinner" &&  !restaurant.menus.dinner.includes(itemDetails)
-  ) {
-    restaurant.menus.dinner.push(itemDetails)
-  } 
-  return restaurant.menus
-}
+  var breakfastType = "breakfast";
+  var lunchType = "lunch";
+  var dinnerType = "dinner";
+  var mealTypes = [breakfastType, lunchType, dinnerType];
+  for(var i = 0; i < mealTypes.length; i++) {
+    if(itemDetails.type === mealTypes[i] && !restaurant.menus[mealTypes[i]].includes(itemDetails)) {
+      restaurant.menus[mealTypes[i]].push(itemDetails);
+    };
+  };
+  return restaurant.menus;
+};
+
 function removeMenuItem(restaurant, itemName, itemType) {
-  var itemDetails = restaurant.menus[itemType]
-  // console.log("current array:", restaurant.menus)
-  for (var i = 0; i< itemDetails.length; i++){
-    if(itemDetails[i].name === itemName)
-    {
-      restaurant.menus[itemType].splice(i,1)
-      // console.log("updated array", restaurant.menus)
-      return`No one is eating our ${itemName} - it has been removed from the ${itemType} menu!`
-    }
-  }
+  var itemDetails = restaurant.menus[itemType];
+  for (var i = 0; i < itemDetails.length; i++) {
+    if(itemDetails[i].name === itemName) {
+      itemDetails.splice(i,1);
+      return `No one is eating our ${itemName} - it has been removed from the ${itemType} menu!`
+    };
+  };
   return `Sorry, we don't sell ${itemName}, try adding a new recipe!`
-}
+};
+
 module.exports = {
   createRestaurant, 
   addMenuItem,
