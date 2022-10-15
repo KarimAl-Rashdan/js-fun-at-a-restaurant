263ad2d1b076bb043bb4dfc71bc77639c4810cc1
karimy <karim.alrashdan14@gmail.com>
2022-10-15 00:49:59 -0600

    Complete checkForFood method

--- src/Chef.js
+++ src/Chef.js
@@ -1,6 +1,33 @@
 class Chef {
-  constructor() {
-    
+  constructor(name, restaurant) {
+    this.name = name;
+    this.restaurant = restaurant;
+  }
+  greetCustomer(name, isMorning) {
+    if(isMorning === true) {
+      return `Good morning, ${name}!`
+    } else {
+    return `Hello, ${name}!`
+    }
+  }
+  checkForFood(foodDetails) {
+    console.log(this.restaurant.menus)
+    var breakfastType = "breakfast"
+    var lunchType = "lunch"
+    var dinnerType = "dinner"
+    var mealTypes = [breakfastType, lunchType, dinnerType]
+    for(var i = 0; i < mealTypes.length; i++) {
+      if(this.restaurant.menus[mealTypes[i]].includes(foodDetails)) {
+        return `Yes, we're serving ${foodDetails.name} today!`
+      } else {
+        return `Sorry, we aren't serving ${foodDetails.name} today.`
+      }
+    }
+    // if(this.restaurant.menus.foodDetails === foodDetails) {
+    // return `Yes, we're serving ${foodDetails.name} today!`
+    // } else {
+    //   return `Sorry, we aren't serving ${foodDetails.name} today.`
+    // }
   }
 }
 
